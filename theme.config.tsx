e175b2e1af29920f285a913d89618437fdda8c4f
cpengilly <29023967+cpengilly@users.noreply.github.com>
2023-10-19 09:33:19 -0700

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -47,7 +47,7 @@ const config: DocsThemeConfig = {
         <meta property="og:title" content={frontMatter.title || 'OP Docs'} />
         <meta
           property="og:description"
-          content={frontMatter.description || 'Build With Optimism Labs'}
+          content={frontMatter.description || 'Build With Optimism'}
         />
       </>
     )
