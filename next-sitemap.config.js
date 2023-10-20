1c9ae21e51e1f42f38717894254ea8adf80b7dc8
cpengilly <29023967+cpengilly@users.noreply.github.com>
2023-10-19 22:04:03 -0700

    style guide and contributing.md updates

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'docs.optimism.io',
+    siteUrl: process.env.SITE_URL || 'https://snazzy-hamster-4e1647.netlify.app/',
     generateRobotsTxt: true, // (optional)
     generateIndexSitemap: true,
     // ...other options
