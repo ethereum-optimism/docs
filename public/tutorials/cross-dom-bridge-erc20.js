d435aa4885fe533d6b3a2000fc30a15782d44b74
Kelvin Fichter <kelvin@optimism.io>
2023-12-01 15:55:11 -0500

    feat: finish updating dapp tutorials to sepolia
    This PR updates all of the dapp developer tutorials to sepolia.

--- public/tutorials/cross-dom-bridge-erc20.js
+++ public/tutorials/cross-dom-bridge-erc20.js
@@ -5,13 +5,13 @@ const ethers = require("ethers")
 
 const privateKey = process.env.TUTORIAL_PRIVATE_KEY
 
-const l1Provider = new ethers.providers.StaticJsonRpcProvider("https://rpc.ankr.com/eth_goerli")
-const l2Provider = new ethers.providers.StaticJsonRpcProvider("https://goerli.optimism.io")
+const l1Provider = new ethers.providers.StaticJsonRpcProvider("https://rpc.ankr.com/eth_sepolia")
+const l2Provider = new ethers.providers.StaticJsonRpcProvider("https://sepolia.optimism.io")
 const l1Wallet = new ethers.Wallet(privateKey, l1Provider)
 const l2Wallet = new ethers.Wallet(privateKey, l2Provider)
 
-const l1Token = "0x32B3b2281717dA83463414af4E8CfB1970E56287"
-const l2Token = "0x3e7eF8f50246f725885102E8238CBba33F276747"
+const l1Token = "0x5589BB8228C07c4e15558875fAf2B859f678d129"
+const l2Token = "0xD08a2917653d4E460893203471f0000826fb4034"
 
 const erc20ABI = [{ constant: true, inputs: [{ name: "_owner", type: "address" }], name: "balanceOf", outputs: [{ name: "balance", type: "uint256" }], type: "function" }, { inputs: [], name: "faucet", outputs: [], stateMutability: "nonpayable", type: "function" }]
 
@@ -27,8 +27,8 @@ console.log((await l1ERC20.balanceOf(l1Wallet.address)).toString())
 const oneToken = 1000000000000000000n
 
 const messenger = new optimism.CrossChainMessenger({
-  l1ChainId: 5,   // 5 for Goerli, 1 for Ethereum
-  l2ChainId: 420, // 420 for OP Goerli, 10 for OP Mainnet
+  l1ChainId: 11155111, // 11155111 for Sepolia, 1 for Ethereum
+  l2ChainId: 11155420, // 11155420 for OP Sepolia, 10 for OP Mainnet
   l1SignerOrProvider: l1Wallet,
   l2SignerOrProvider: l2Wallet,
 })
