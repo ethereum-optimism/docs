a834df33b92a4146138fe01186919060da26438f
Tiago Alves <alvesjtiago@gmail.com>
2024-08-01 23:49:41 +0300

    Upload index to Algolia script and Github action

--- utils/algolia-indexer.ts
+++ utils/algolia-indexer.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import matter from "gray-matter";
+import algoliasearch from "algoliasearch";
 
 const globby = require("globby");
 
@@ -11,10 +12,7 @@ const globby = require("globby");
     "!pages/_app.mdx",
   ];
 
-  const pages = await globby([
-    "pages/",
-    ...EXCLUDE_FILES,
-  ]);
+  const pages = await globby(["pages/", ...EXCLUDE_FILES]);
 
   const objects = pages.map((page: string) => {
     const fileContents = fs.readFileSync(page, "utf8");
@@ -26,11 +24,20 @@ const globby = require("globby");
     const sections = content.split(/^(?=#+ )/m).filter(Boolean);
 
     const pageObjects = sections.map((section) => {
+      // There's a limit of 10kb on the free plan for each record so this adjustment on the
+      // content is for larger sections that go beyond this value including slug, title, etc.
+      // Ideally this would be further broken down or the plan increased.
+      let encoder = new TextEncoder();
+      let decoder = new TextDecoder("utf-8");
+      let uint8 = encoder.encode(section);
+      let slicedSection = uint8.slice(0, 9500);
+      let sectionSized = decoder.decode(slicedSection);
+
       return {
         ...data,
         section: slug,
         slug,
-        content: section,
+        content: sectionSized,
       };
     });
 
@@ -39,9 +46,23 @@ const globby = require("globby");
 
   const flattenedObjects = objects.flat(Infinity);
 
-  fs.writeFile("./algolia.json", JSON.stringify(flattenedObjects), (err) => {
-    if (err) {
-      console.error("Error writing algolia.json:", err);
-    }
-  });
+  const client = algoliasearch(
+    process.env.ALGOLIA_APPLICATION_ID ?? "",
+    process.env.ALGOLIA_WRITE_API_KEY ?? ""
+  );
+
+  const index = client.initIndex(process.env.ALGOLIA_INDEX_NAME ?? "");
+
+  console.log("Uploading index...");
+
+  try {
+    await index.replaceAllObjects(flattenedObjects, {
+      safe: true,
+      autoGenerateObjectIDIfNotExist: true,
+    });
+
+    console.log("Indexing completed!");
+  } catch (e) {
+    console.log("Error uploading index", e);
+  }
 })();
