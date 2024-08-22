bffbf3f01163df342ec87efe659b4b5b04e0ddeb
Blessing Krofegha <bkrofegha@gmail.com>
2024-08-22 14:01:07 +0100

    Add callout to interop

--- components/WipCallout.tsx
+++ components/WipCallout.tsx
@@ -37,3 +37,24 @@ export function WipCallout({ context }: Props): ReactElement {
     </div>
   );
 }
+
+export function InfoCallout({ context }: Props): ReactElement {
+  return (
+    <div className="custom-callouts nx-w-full nx-mt-6 nx-flex nx-justify-center nx-items-center nx-bg-white dark:nx-bg-black">
+      <div className="nx-w-full  nx-px-4 nx-text-center nx-font-medium nx-text-sm nx-text-left">
+        {context ? (
+          context
+        ) : (
+          <div className="nx-text-left">
+            Interop is currently in <strong>active development</strong> and not yet ready for production use. 
+            The information provided here may change frequently. 
+            <p>
+            We recommend checking back 
+            regularly for the most up-to-date information.
+            </p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
