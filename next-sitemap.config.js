14977419c78a9291e965f214233fbb6790b56634
Maura Wilder <560657+squdgy@users.noreply.github.com>
2024-10-16 08:05:51 -0400

    Changes for Metal L2 references

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://docs.optimism.io/',
+  siteUrl: process.env.SITE_URL || 'https://docs.metall2.com/',
   generateRobotsTxt: true, // (optional)
   generateIndexSitemap: true,
   // ...other options
