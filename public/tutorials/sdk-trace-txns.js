840fa609ed01a6d11ac16bf6175bd7e228ee4314
Blessing Krofegha <bkrofegha@gmail.com>
2024-10-22 18:14:36 +0100

    updated codebase

--- public/tutorials/sdk-trace-txns.js
+++ public/tutorials/sdk-trace-txns.js
@@ -1,20 +1,21 @@
 (async () => {
 
   const { createPublicClient, http } = require('viem');
+  const { optimismSepolia, sepolia } = require('viem/chains');
 
 // Need to use Alchemy or something here because the getDepositsByAddress and
 // getWithdrawalsByAddress functions use a large block range that the public RPC doesn't support
 // because it uses Ankr. Maybe the SDK should be updated to use smaller block ranges depending
 // on the RPC but that's a separate issue.
-const l1RpcUrl = process.env.L1_RPC_URL;
-const l2RpcUrl = process.env.L2_RPC_URL;
+const l1RpcUrl = "https://rpc.ankr.com/eth_sepolia";
+const l2RpcUrl = "https://sepolia.optimism.io";
 
 // Docs CI wallet, will have deposits and withdrawals.
 const depositHash = '0x5896d6e4a47b465e0d925723bab838c62ef53468139a5e9ba501efd70f90cccb'
 const withdrawalHash = '0x18b8b4022b8d9e380fd89417a2e897adadf31e4f41ca17442870bf89ad024f42'
 
 const l1Client = createPublicClient({
-  chain: optimism,
+  chain: sepolia,
   transport: http(l1RpcUrl),
 });
 
