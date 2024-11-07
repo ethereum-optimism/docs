cc77c5753d7918f239a204cf339d0e81748becab
soyboy <soyboy@oplabs.co>
2024-11-07 14:57:55 -0800

    adding predeploys and making the callout closable

--- components/WipCallout.tsx
+++ components/WipCallout.tsx
@@ -51,9 +51,14 @@ export function WipCallout({ context }: Props): ReactElement {
   );
 }
 
-export function InfoCallout({ context }: Props): ReactElement {
+export function InteropCallout({ context }: Props): ReactElement {
+  const [closeCallout, setCloseCallout] = useState(false);
   return (
-    <div className="custom-callouts nx-w-full nx-mt-6 nx-flex nx-justify-center nx-items-center nx-bg-white dark:nx-bg-black">
+    <div
+      className={`custom-callouts nx-w-full nx-mt-6 nx-flex nx-justify-center nx-items-center nx-bg-white dark:nx-bg-black ${
+        closeCallout && 'nx-hidden'
+      }`}
+    >
       <div className="nx-w-full  nx-px-4 nx-text-center nx-font-medium nx-text-sm nx-text-left">
         {context ? (
           context
@@ -69,6 +74,12 @@ export function InfoCallout({ context }: Props): ReactElement {
           </div>
         )}
       </div>
+      <button
+        className="callout-close-btn"
+        onClick={() => setCloseCallout(true)}
+      >
+        x
+      </button>
     </div>
   );
 }
