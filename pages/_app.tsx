1e38dbb6cd77524d5855a0be81740c2086db83ec
Imran Jami <imranajami@gmail.com>
2025-01-31 11:04:29 -0800

    adding growthbook

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,45 +1,47 @@
-import '../styles/global.css'
+import '../styles/global.css';
 
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
-import * as gtag from '../utils/gtag'
-import * as aa from "search-insights"
-import AlgoliaContext from '@/utils/contexts/AlgoliaContext'
-import ScrollDispatcher from '@/components/ScrollDispatcher'
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import * as gtag from '../utils/gtag';
+import * as aa from 'search-insights';
+import AlgoliaContext from '@/utils/contexts/AlgoliaContext';
+import ScrollDispatcher from '@/components/ScrollDispatcher';
+import { CustomGrowthBookProvider } from '../providers/GrowthbookProvider';
 
 export default function App({ Component, pageProps }) {
   const [queryID, setQueryID] = useState(null);
   const [objectID, setObjectID] = useState(null);
 
-  const router = useRouter()
+  const router = useRouter();
   useEffect(() => {
     const handleRouteChange = (url) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   aa.default('init', {
-    appId: "JCF9BUJTB9",
-    apiKey: "cc766a73d4b0004e3059677de49297a2"
-  })
+    appId: 'JCF9BUJTB9',
+    apiKey: 'cc766a73d4b0004e3059677de49297a2'
+  });
 
   return (
-    <AlgoliaContext.Provider 
-      value={{
-        queryID,
-        setQueryID,
-        objectID,
-        setObjectID,
-      }}
-    >
-      <ScrollDispatcher>
-        <Component {...pageProps} />
-      </ScrollDispatcher>
-    </AlgoliaContext.Provider>
-  )
+    <CustomGrowthBookProvider>
+      <AlgoliaContext.Provider
+        value={{
+          queryID,
+          setQueryID,
+          objectID,
+          setObjectID
+        }}
+      >
+        <ScrollDispatcher>
+          <Component {...pageProps} />
+        </ScrollDispatcher>
+      </AlgoliaContext.Provider>
+    </CustomGrowthBookProvider>
+  );
 }
-
