a47ce5e0ce1c03720dce9d95549873f467e41443
Blessing Krofegha <bkrofegha@gmail.com>
2025-03-10 23:22:03 +0100

    update tut

--- public/tutorials/cross-dom-bridge-erc20.js
+++ public/tutorials/cross-dom-bridge-erc20.js
@@ -5,14 +5,27 @@
   const { privateKeyToAccount } = require('viem/accounts');
   const { depositERC20 } = require('@eth-optimism/viem');
 
+  // Define token addresses and other configuration
+const PRIVATE_KEY = process.env.TUTORIAL_PRIVATE_KEY
+const L1_RPC_URL = 'https://rpc.ankr.com/eth_sepolia'
+const L2_RPC_URL = 'https://sepolia.optimism.io'
+
+const l1Token = "0x5589BB8228C07c4e15558875fAf2B859f678d129"
+const l2Token = "0xD08a2917653d4E460893203471f0000826fb4034"
+
+const erc20ABI = [{ constant: true, inputs: [{ name: "_owner", type: "address" }], name: "balanceOf", outputs: [{ name: "balance", type: "uint256" }], type: "function" }, { inputs: [], name: "faucet", outputs: [], stateMutability: "nonpayable", type: "function" }]
+
+// Create your account from private key
+const account = privateKeyToAccount(PRIVATE_KEY)
+
   const l1Provider = createPublicClient({
     chain: sepolia,
-    transport: http("https://rpc.ankr.com/eth_sepolia")
+    transport: http(L1_RPC_URL)
   })
   
   const l2Provider = createPublicClient({
     chain: optimismSepolia,
-    transport: http("https://sepolia.optimism.io")
+    transport: http(L2_RPC_URL)
   })
 
   const l1Wallet = createWalletClient({
@@ -27,16 +40,101 @@
     transport: http("https://sepolia.optimism.io"),
   });
 
+  const walletAddress = account.address
+
+  const oneToken = 1000000000000000000n
+
+  // Check L1 token balance
+  const l1Balance = await l1Provider.readContract({
+    address: l1Token,
+    abi: erc20ABI,
+    functionName: 'balanceOf',
+    args: [walletAddress]
+  })
+  console.log(`Initial L1 balance: ${l1Balance}`)
+
+  // Check L2 token balance
+  const l2Balance = await l2Provider.readContract({
+    address: l2Token,
+    abi: erc20ABI,
+    functionName: 'balanceOf',
+    args: [walletAddress]
+  })
+  console.log(`Initial L2 balance: ${l2Balance}`)
 
 
-const PRIVATE_KEY = process.env.TUTORIAL_PRIVATE_KEY
-const account = privateKeyToAccount(PRIVATE_KEY)
+  // Define the amount to bridge
+const amountToBridge = parseEther('0.01') // Adjust based on token decimals
 
+// Check current allowance
+const currentAllowance = await l1Provider.readContract({
+  address: l1Token,
+  abi: erc20ABI,
+  functionName: 'allowance',
+  args: [walletAddress, '0x3154Cf16ccdb4C6d922629664174b904d80F2C35'] // L1 Standard Bridge
+})
 
-const l1Token = "0x5589BB8228C07c4e15558875fAf2B859f678d129"
-const l2Token = "0xD08a2917653d4E460893203471f0000826fb4034"
+// Approve tokens if needed
+if (currentAllowance < amountToBridge) {
+  console.log('Approving tokens for transfer...')
+  const approvalTx = await l1Wallet.writeContract({
+    address: l1Token,
+    abi: erc20ABI,
+    functionName: 'approve',
+    args: ['0x3154Cf16ccdb4C6d922629664174b904d80F2C35', amountToBridge]
+  })
+  
+  console.log(`Approval transaction hash: ${approvalTx}`)
+  await l1PublicClient.waitForTransactionReceipt({ hash: approvalTx })
+  console.log('Token approval complete')
+}
 
-const erc20ABI = [{ constant: true, inputs: [{ name: "_owner", type: "address" }], name: "balanceOf", outputs: [{ name: "balance", type: "uint256" }], type: "function" }, { inputs: [], name: "faucet", outputs: [], stateMutability: "nonpayable", type: "function" }]
+console.log(`Bridging ${amountToBridge} tokens to L2...`)
+
+const depositTx = await depositERC20(l1Wallet, {
+  tokenAddress: l1Token,
+  remoteTokenAddress: l2Token,
+  amount: amountToBridge,
+  targetChain: optimism,
+  minGasLimit: 200000 // Recommended minimum gas limit
+})
+
+console.log(`Deposit transaction hash: ${depositTx}`)
+
+console.log('Waiting for deposit transaction to be confirmed on L1...')
+const depositReceipt = await l1Provider.waitForTransactionReceipt({ 
+  hash: depositTx,
+  confirmations: 1
+})
+console.log(`Deposit confirmed in block ${depositReceipt.blockNumber}`)
+
+console.log('Waiting for message to be relayed to L2...')
+console.log('This may take 1-5 minutes on mainnet...')
+
+console.log('Waiting for message to be relayable...')
+await l1Provider.getWithdrawalStatus({ 
+  receipt, 
+  targetChain: l2Provider.chain, 
+}) 
+
+console.log('Relaying message...')
+const [message] = getWithdrawals(receipt)
+await l1Provider.waitToFinalize({ withdrawalHash: message.withdrawalHash, targetChain: l2Provider.chain }) 
+
+// console.log('Message status:', result.status)
+// console.log('L2 transaction hash:', result.transactionReceipt?.transactionHash)
+
+const l2BalanceAfter = await l2Provider.readContract({
+  address: l1Token,
+  abi: erc20ABI,
+  functionName: 'balanceOf',
+  args: [walletAddress]
+})
+
+console.log(`Final L2 balance: ${l2BalanceAfter}`)
+console.log(`Increase: ${l2BalanceAfter - l2Balance}`)
+console.log('Token bridging complete!')
+
 
 const l1ERC20 = getContract({
   address: l1Token,
@@ -44,79 +142,5 @@ const l1ERC20 = getContract({
   client: l1Wallet
 })
 
-// const l1ERC20 = new ethers.Contract(l1Token, erc20ABI, l1Wallet)
-
-console.log('Getting L1 tokens from faucet...')  
-tx = await l1ERC20.faucet()
-await tx.wait()
-
-console.log('L1 balance:')
-console.log((await l1ERC20.balanceOf(l1Wallet.address)).toString())
-
-const oneToken = 1000000000000000000n
-
-const messenger = new optimism.CrossChainMessenger({
-  l1ChainId: 11155111, // 11155111 for Sepolia, 1 for Ethereum
-  l2ChainId: 11155420, // 11155420 for OP Sepolia, 10 for OP Mainnet
-  l1SignerOrProvider: l1Wallet,
-  l2SignerOrProvider: l2Wallet,
-})
-
-console.log('Approving L1 tokens for deposit...')
-tx = await messenger.approveERC20(l1Token, l2Token, oneToken)
-await tx.wait()
-
-console.log('Depositing L1 tokens...')
-tx = await messenger.depositERC20(l1Token, l2Token, oneToken)
-await tx.wait()
-
-console.log('Waiting for deposit to be relayed...')
-await messenger.waitForMessageStatus(tx.hash, optimism.MessageStatus.RELAYED)
-
-console.log('L1 balance:')
-console.log((await l1ERC20.balanceOf(l1Wallet.address)).toString())
-
-const l2ERC20 = new ethers.Contract(l2Token, erc20ABI, l2Wallet)
-
-console.log('L2 balance:')
-console.log((await l2ERC20.balanceOf(l2Wallet.address)).toString())
-
-console.log('Withdrawing L2 tokens...')
-const withdrawal = await messenger.withdrawERC20(l1Token, l2Token, oneToken)
-await withdrawal.wait()
-
-console.log('L2 balance:')
-console.log((await l2ERC20.balanceOf(l2Wallet.address)).toString())
-
-console.log('Waiting for withdrawal to be provable...')
-await messenger.waitForMessageStatus(withdrawal.hash, optimism.MessageStatus.READY_TO_PROVE)
-
-console.log('Proving withdrawal...')
-await messenger.proveMessage(withdrawal.hash)
-
-console.log('Waiting for withdrawal to be relayable...')
-await messenger.waitForMessageStatus(withdrawal.hash, optimism.MessageStatus.READY_FOR_RELAY)
-
-// Wait for the next block to be produced, only necessary for CI because messenger can return
-// READY_FOR_RELAY before the RPC we're using is caught up to the latest block. Waiting for an
-// additional block ensures that the RPC is caught up and the message can be relayed. Users
-// should not need to do this when running the tutorial.
-const maxWaitTime = Date.now() + 120000 // 2 minutes in milliseconds
-const currentBlock = await l1Provider.getBlockNumber()
-while (await l1Provider.getBlockNumber() < currentBlock + 1) {
-  if (Date.now() > maxWaitTime) {
-    throw new Error('Timed out waiting for block to be produced')
-  }
-  await new Promise(resolve => setTimeout(resolve, 1000))
-}
-
-console.log('Relaying withdrawal...')
-await messenger.finalizeMessage(withdrawal.hash)
-
-console.log('Waiting for withdrawal to be relayed...')
-await messenger.waitForMessageStatus(withdrawal.hash, optimism.MessageStatus.RELAYED)
-
-console.log('L1 balance:')
-console.log((await l1ERC20.balanceOf(l1Wallet.address)).toString())
 
 })()
