45094211d59828352da3b5228d3843c6210dc0c6
Blessing Krofegha <bkrofegha@gmail.com>
2025-03-27 21:12:47 +0100

    Update standard-bridge-custom-token.sol

--- public/tutorials/standard-bridge-custom-token.sol
+++ public/tutorials/standard-bridge-custom-token.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.15;
+pragma solidity 0.8.20;
 
 // Import the standard ERC20 implementation from OpenZeppelin
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
