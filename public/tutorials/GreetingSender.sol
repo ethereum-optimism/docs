dcf96d0ac0bbca93503d3708cda6c5bcbed66b12
Ori Pomerantz <qbzzt1@gmail.com>
2025-04-25 14:37:17 -0500

    Ready

--- public/tutorials/GreetingSender.sol
+++ public/tutorials/GreetingSender.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
  
 import { Predeploys } from "@eth-optimism/contracts-bedrock/src/libraries/Predeploys.sol";
-import { IL2ToL2CrossDomainMessenger } from "@eth-optimism/packages/contracts-bedrock/interfaces/L2/IL2ToL2CrossDomainMessenger.sol";
+import { IL2ToL2CrossDomainMessenger } from "@eth-optimism/contracts-bedrock/interfaces/L2/IL2ToL2CrossDomainMessenger.sol";
  
 import { Greeter } from "src/Greeter.sol";
  
