89a868be75561667e6cfc0d7a676b3410d884745
krofax <bkrofegha@gmail.com>
2025-07-11 18:29:31 +0100

    updated the structure of the docs

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://docs.optimism.io/',
-  generateRobotsTxt: true, // (optional)
-  generateIndexSitemap: true,
-  // ...other options
-}
